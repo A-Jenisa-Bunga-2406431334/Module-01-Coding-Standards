5d9212e22fc5001558843f4decc1bb05ff5e21b5
jebs-tech <bungasaragih688@gmail.com>
2026-02-10 10:10:22 +0700

    Add testing dependencies to build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,21 @@
 plugins {
 	java
-	id("org.springframework.boot") version "3.5.10"
-	id("io.spring.dependency-management") version "1.1.7"
+	id("org.springframework.boot") version "3.2.2"
+	id("io.spring.dependency-management") version "1.1.4"
 }
 
 group = "id.ac.ui.cs.advprog"
 version = "0.0.1-SNAPSHOT"
-description = "Demo project for Spring Boot"
 
 java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(21)
-	}
+	sourceCompatibility = JavaVersion.VERSION_21
 }
+
+// 1. Tambahkan variables ini
+val seleniumJavaVersion = "4.14.1"
+val seleniumJupiterVersion = "5.0.1"
+val webdrivermanagerVersion = "5.6.3"
+val junitJupiterVersion = "5.9.1"
 
 configurations {
 	compileOnly {
@@ -32,9 +35,35 @@ dependencies {
 	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	
+	// 2. Tambahkan dependencies untuk testing
+	testImplementation("org.seleniumhq.selenium:selenium-java:$seleniumJavaVersion")
+	testImplementation("io.github.bonigarcia:selenium-jupiter:$seleniumJupiterVersion")
+	testImplementation("io.github.bonigarcia:webdrivermanager:$webdrivermanagerVersion")
+	testImplementation("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
+	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
 }
 
-tasks.withType<Test> {
+// 3. Tambahkan kode setelah dependencies
+tasks.register<Test>("unitTest") {
+	description = "Runs unit tests."
+	group = "verification"
+	
+	filter {
+		excludeTestsMatching("*FunctionalTest")
+	}
+}
+
+tasks.register<Test>("functionalTest") {
+	description = "Runs functional tests."
+	group = "verification"
+	
+	filter {
+		includeTestsMatching("*FunctionalTest")
+	}
+}
+
+// 4. Replace bagian terakhir dengan kode ini
+tasks.withType<Test>().configureEach {
 	useJUnitPlatform()
 }
