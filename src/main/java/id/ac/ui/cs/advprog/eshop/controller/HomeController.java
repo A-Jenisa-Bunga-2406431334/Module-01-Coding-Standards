da746a619dc963349bf919d386a17933c41d6e5b
jebs-tech <bungasaragih688@gmail.com>
2026-02-24 11:22:47 +0700

    Fix PMD violations

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/HomeController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/HomeController.java
@@ -3,16 +3,11 @@ package id.ac.ui.cs.advprog.eshop.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-// Trigger PMD scan
 @Controller
 public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        try {
-            int x = 1 / 0;
-        } catch (Exception e) {
-        }
         return "home";
     }
 }
