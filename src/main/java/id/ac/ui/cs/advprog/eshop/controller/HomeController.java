035a7aaf3aa116ae3ef2c3a0858be0c2a10bdbd8
jebs-tech <bungasaragih688@gmail.com>
2026-02-24 11:06:32 +0700

    Trigger PMD scan

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/HomeController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/HomeController.java
@@ -3,6 +3,7 @@ package id.ac.ui.cs.advprog.eshop.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+// Trigger PMD scan
 @Controller
 public class HomeController {
 
@@ -11,3 +12,4 @@ public class HomeController {
         return "home";
     }
 }
+
