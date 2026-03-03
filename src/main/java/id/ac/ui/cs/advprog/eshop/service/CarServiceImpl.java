66ccc4f016c2c892a31ab25779417222c16c4e6d
jebs-tech <bungasaragih688@gmail.com>
2026-03-03 14:44:21 +0700

    refactor: clean up CarServiceImpl (remove unused import and TODO comments)

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/CarServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/CarServiceImpl.java
@@ -1,8 +1,6 @@
 package id.ac.ui.cs.advprog.eshop.service;
-
 import id.ac.ui.cs.advprog.eshop.model.Car;
 import id.ac.ui.cs.advprog.eshop.repository.CarRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -15,10 +13,9 @@ public class CarServiceImpl implements CarService {
     public CarServiceImpl(CarRepository carRepository) {
         this.carRepository = carRepository;
     }
-    
+
     @Override
     public Car create(Car car) {
-        // TODO Auto-generated method stub
         carRepository.create(car);
         return car;
     }
@@ -33,13 +30,11 @@ public class CarServiceImpl implements CarService {
 
     @Override
     public Car findById(String carId) {
-        Car car = carRepository.findById(carId);
-        return car;
+        return carRepository.findById(carId);
     }
 
     @Override
     public void update(String carId, Car car) {
-        // TODO Auto-generated method stub
         carRepository.update(carId, car);
     }
 
