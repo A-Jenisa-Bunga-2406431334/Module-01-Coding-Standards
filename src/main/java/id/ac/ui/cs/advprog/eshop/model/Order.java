c9d0e18381a47481f43e878e015cd8b9a3f1bb7f
jebs-tech <bungasaragih688@gmail.com>
2026-03-10 16:33:48 +0700

    [REFACTOR] Apply OrderStatus enum check to Order model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Order.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Order.java
@@ -1,6 +1,7 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
-import java.util.Arrays;
+import id.ac.ui.cs.advprog.eshop.enums.OrderStatus;
+
 import java.util.List;
 
 public class Order {
@@ -10,12 +11,12 @@ public class Order {
     String author;
     String status;
 
-    // Step 2: Constructor tanpa status parameter
+    // Constructor tanpa status (default WAITING_PAYMENT)
     public Order(String id, List<Product> products, Long orderTime, String author) {
         this.id = id;
         this.orderTime = orderTime;
         this.author = author;
-        this.status = "WAITING_PAYMENT";
+        this.status = OrderStatus.WAITING_PAYMENT.getValue();
         
         if (products.isEmpty()) {
             throw new IllegalArgumentException();
@@ -24,25 +25,18 @@ public class Order {
         }
     }
 
-    // Step 4: Constructor dengan status parameter
+    // Constructor dengan status
     public Order(String id, List<Product> products, Long orderTime, String author, String status) {
         this(id, products, orderTime, author);
-        
-        String[] statusList = {"WAITING_PAYMENT", "FAILED", "SUCCESS", "CANCELLED"};
-        if (Arrays.stream(statusList).noneMatch(item -> (item.equals(status)))) {
-            throw new IllegalArgumentException();
-        } else {
-            this.status = status;
-        }
+        this.setStatus(status);
     }
 
-    // Step 7: setStatus method
+    // setStatus dengan validasi pakai Enum
     public void setStatus(String status) {
-        String[] statusList = {"WAITING_PAYMENT", "FAILED", "SUCCESS", "CANCELLED"};
-        if (Arrays.stream(statusList).noneMatch(item -> (item.equals(status)))) {
-            throw new IllegalArgumentException();
-        } else {
+        if (OrderStatus.contains(status)) {
             this.status = status;
+        } else {
+            throw new IllegalArgumentException();
         }
     }
 
